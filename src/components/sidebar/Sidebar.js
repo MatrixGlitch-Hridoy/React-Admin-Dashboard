1cdbaab249eeec18274dc775bbb3b256f5d19750
MatrixGlitch-Hridoy <rkhridoy68@gmail.com>
2022-05-19 22:08:41 +0600

    sidebar design ongoing

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './sidebar.scss'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='top'>
+                <span className='logo'>rkadmin</span>
+            </div>
+            <hr/>
+            <div className='center'>
+                <ul>
+                    <li>
+                        <DashboardIcon/>
+                        <span>Dashboard</span>
+                    </li>
+                    <li>
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <span>Delivery</span>
+                    </li>
+                    <li>
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <span>Notifications</span>
+                    </li>
+                    <li>
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <span>Settings</span>
+                    </li>
+                    <li>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <span>Logout</span>
+                    </li>
+                    
+                </ul>
+            </div>
+            <div className='bottom'>color options</div>
+        </div>
+    );
+};
+
+export default Sidebar;
